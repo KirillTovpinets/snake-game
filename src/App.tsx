76b202512e9c4a54e6fea3a19be27f5b9aa391a9
Kirill Tovpinets <kirill.tovpinets.con@viacom18.com>
2023-09-28 12:30:45 +0200

    fix touch fraction

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,49 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { GameField } from './components/GameField'
 import { GameSettings } from './components/GameSettings/GameSettings'
+import { DIRECTIONS, MAX_DELTA_FRACTION } from './constants'
 import { setSnakeDirection } from './features/game'
 import { RootState } from './store'
 import { KeyboardCodes } from './types'
-import { DIRECTIONS } from './constants'
 
 const App: React.FC = () => {
   const dispatch = useDispatch()
   const timerId = useSelector((state: RootState) => state.game.timerId)
-  const isInProgress = useSelector((state: RootState) => state.game.isInProgress);
+  const isInProgress = useSelector(
+    (state: RootState) => state.game.isInProgress
+  )
 
   useEffect(() => {
     const keydownHanlder = (event: KeyboardEvent) => {
-      if(isInProgress || !DIRECTIONS.includes(event.code as KeyboardCodes)){
-        return;
+      if (isInProgress || !DIRECTIONS.includes(event.code as KeyboardCodes)) {
+        return
       }
       dispatch(setSnakeDirection(event.code as KeyboardCodes))
     }
     window.document.addEventListener('keydown', keydownHanlder)
 
-    let startX: number;
-    let startY: number;
+    let startX: number
+    let startY: number
     document.addEventListener('touchstart', (event: TouchEvent) => {
-      const touch = event.changedTouches[0];
+      const touch = event.changedTouches[0]
 
-      startX = touch.pageX;
-      startY = touch.pageY;
+      startX = touch.pageX
+      startY = touch.pageY
     })
 
     document.addEventListener('touchend', (event: TouchEvent) => {
-      const touch = event.changedTouches[0];
-      const deltaX = touch.pageX - startX;
-      const deltaY = touch.pageY - startY;
-      if( deltaX> 0) {
+      const touch = event.changedTouches[0]
+      const deltaX = touch.pageX - startX
+      const deltaY = touch.pageY - startY
+      const isXFractionOk = Math.abs(deltaX) < MAX_DELTA_FRACTION
+      const isYFractionOk = Math.abs(deltaY) < MAX_DELTA_FRACTION
+      if (deltaX > 0 && !isXFractionOk) {
         dispatch(setSnakeDirection(KeyboardCodes.right))
-      } else if(touch.pageX - startX < 0) {
+      } else if (deltaX < 0 && !isXFractionOk) {
         dispatch(setSnakeDirection(KeyboardCodes.left))
-      } else if(touch.pageY - startY < 0) {
+      } else if (deltaY < 0 && !isYFractionOk) {
         dispatch(setSnakeDirection(KeyboardCodes.up))
-      } else if(touch.pageY - startY > 0) {
+      } else if (deltaY > 0 && !isYFractionOk) {
         dispatch(setSnakeDirection(KeyboardCodes.down))
       }
     })
--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,14 @@
-import { KeyboardCodes } from "./types"
+import { KeyboardCodes } from './types'
 
 export const SPEED_STEP_IN_MILISECONDS = 50
 export const INITIAL_SPEED_IN_MILISECONDS = 1000
 export const TESTING_SPEED_IN_MILISECONDS = 100
 export const SPEED_MINIMUM_IN_MILISECONDS = 100
 export const CELL_WIDTH_IN_PERCENTAGE = 4
-
-export const DIRECTIONS: KeyboardCodes[] = [KeyboardCodes.down, KeyboardCodes.left, KeyboardCodes.up, KeyboardCodes.right]
+export const MAX_DELTA_FRACTION = 20
+export const DIRECTIONS: KeyboardCodes[] = [
+  KeyboardCodes.down,
+  KeyboardCodes.left,
+  KeyboardCodes.up,
+  KeyboardCodes.right,
+]
