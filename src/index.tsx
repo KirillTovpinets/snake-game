0e2117e28c2dba11e7b6be5fdde8a8b74536b003
KirillTovpinets <kirill.tovpinets@icloud.com>
2023-12-14 13:44:46 +0300

    fixed index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import "@repo/ui/dist/esm/dist/main.css";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 
-import App from './App';
-import './index.css';
-import store from './store';
+import App from "./App";
+import "./index.css";
+import store from "./store";
 
 const SnakeGame = () => (
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
 
-export default SnakeGame
+export default SnakeGame;
